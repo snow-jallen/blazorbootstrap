69c252e87d928a7429ddebe6c70905e6f23438e7
Jonathan Allen <jallen@snow.edu>
2023-12-05 11:03:20 -0700

    .net 7

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -8,7 +8,7 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
-        var actual = GpaCalculator.Calculate([new Course("C1", 4, 3), new Course("C2", 3.5M, 1)]);
+        var actual = GpaCalculator.Calculate(new[] { new Course("C1", 4, 3), new Course("C2", 3.5M, 1) });
         actual.Should().Be(((4 * 3) + (3.5M * 1)) / 4);
     }
 }
